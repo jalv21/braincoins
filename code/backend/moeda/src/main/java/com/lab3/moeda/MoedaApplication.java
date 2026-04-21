02688ae054f63054c8fac1301e30464e09ca99c2
Jalv <araujo.ja.rodrigues@gmail.com>
2026-04-20 22:22:17 -0300

    chore/inicializando projeto backend Spring Boot

---
+++ code/backend/moeda/src/main/java/com/lab3/moeda/MoedaApplication.java
@@ -0,0 +1,13 @@
+package com.lab3.moeda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoedaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoedaApplication.class, args);
+		System.out.println("hello, world!");
+	}
+}
